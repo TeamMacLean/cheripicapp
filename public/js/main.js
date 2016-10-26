3860dda8fdff2c68fefcde3ccd7af51a084c2e78
Martin Page <wookoouk@gmail.com>
2016-10-26 17:21:16 +0100

    start

---
+++ public/js/main.js
@@ -0,0 +1,22 @@
+// $(function () {
+//     $("#form").submit(function (e) {
+//
+//         var url = "path/to/your/script.php"; // the script where you handle the form input.
+//
+//         var formData = $("#form").serializeArray();
+//         console.log(formData);
+//
+//         $.ajax({
+//             type: "POST",
+//             url: url,
+//             data: formData, // serializes the form's elements.
+//             success: function (data) {
+//                 alert(data); // show response from the php script.
+//             }
+//         });
+//
+//         e.preventDefault(); // avoid to execute the actual submit of the form.
+//     });
+// })
+
+
--- server.js
+++ server.js
@@ -1,36 +1,13 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const routes = require('./routes');
-const app = express();
+const app = require('./app');
 const config = require('./config.json');
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-
-
 var server = require('http').Server(app);
-const io = require('socket.io')(server);
-io.on('connection', socket => {
-    socket.on('submit', function () {
-        var exec = require("child_process").exec;
-
-        exec('cheripic -f assembly.fa -a mutbulk.pileup -b bgbulk.pileup --output=cheripic_output', function (err, stdout, stderr) {
-            console.log(stdout);
-
-
-        });
-    });
-});
+// const io = require('socket.io')(server);
+// io.on('connection', socket => {
+//     socket.on('submit', function () {
+//
+//     });
+// });
 
 
 server.listen(config.port, function () {
